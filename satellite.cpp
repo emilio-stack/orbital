4f2814b52a6ce3bfe8ba4532d0d57b4de0e7b08d
emilio-stack <84351829+emilio-stack@users.noreply.github.com>
2022-06-04 10:10:54 -0600

    Adding a basic satellite

---
+++ satellite.cpp
@@ -0,0 +1,8 @@
+//
+//  satellite.cpp
+//  Lab07
+//
+//  Created by Emilio on 6/4/22.
+//
+
+#include <stdio.h>
---
+++ satellite.h
@@ -0,0 +1,12 @@
+//
+//  satellite.h
+//  Lab07
+//
+//  Created by Emilio on 6/4/22.
+//
+
+#ifndef satellite_h
+#define satellite_h
+
+
+#endif /* satellite_h */
